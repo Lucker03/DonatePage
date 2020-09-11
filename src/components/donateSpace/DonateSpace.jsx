7bc40cbeb17c8275cb3bfd7921e61e456b530526
Noah <noah.dahlhaus@tobit.com>
2020-09-11 14:54:03 +0200

    :zap: Add new Style

--- src/components/donateSpace/DonateSpace.jsx
+++ src/components/donateSpace/DonateSpace.jsx
@@ -6,22 +6,28 @@ import { Button, Input, Checkbox, RadioButton } from 'chayns-components/lib';
 // Styles
 import './DonateSpace.scss';
 
-const DonateSpace = () => (
-    <div className="DonateContainer">
-        <div className="DonateAmountContainer">
-            <RadioButton name="donate" className="content">1€</RadioButton>
-            <RadioButton name="donate" className="content">2€</RadioButton>
-            <RadioButton name="donate" className="content">5€</RadioButton>
-            <RadioButton name="donate" className="content">10€</RadioButton>
-            <RadioButton name="donate" className="content"><Input placeholder="Eigener Betrag"/></RadioButton>
-            <div className="content"/>
-            <div className="content">Anonym spenden</div>
-            <Checkbox toggleButton="true"/>
+const DonateSpace = () => {
+    const thanks = () => {
+        chayns.dialog.alert('', 'Danke für die Spende');
+    };
+
+    return (
+        <div className="DonateContainer">
+            <div className="DonateAmountContainer">
+                <RadioButton name="donate" className="content">1€</RadioButton>
+                <RadioButton name="donate" className="content">2€</RadioButton>
+                <RadioButton name="donate" className="content">5€</RadioButton>
+                <RadioButton name="donate" className="content">10€</RadioButton>
+                <RadioButton name="donate" className="content"><Input type="number" dynamic placeholder="Eigener Betrag"/></RadioButton>
+                <div className="content"/>
+                <div className="content">Anonym spenden</div>
+                <Checkbox toggleButton="true"/>
+            </div>
+            <div className="center">
+                <Button className="center" onClick={thanks}>Spenden</Button>
+            </div>
         </div>
-        <div className="center">
-            <Button className="center">Spenden</Button>
-        </div>
-    </div>
-);
+    );
+};
 
 export default DonateSpace;
